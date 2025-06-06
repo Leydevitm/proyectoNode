3399e8bba7c1a626bf1258cc95fd0abed656047c
leydevitm <leivy.candelaria@devitm.com>
2025-06-06 11:51:50 -0600

    Clase 24 Callback Hell

--- seccion3/Fundamentos/callback-hell.js
+++ seccion3/Fundamentos/callback-hell.js
@@ -34,20 +34,33 @@ const getEmpleado=(id, callback) =>{
 
     if(empleado){
      // return empleado;
-      callback(null,empleado);
+      callback(null,empleado.nombre);
     }else{
         //return `Empleado con id ${id} no existe`;
          callback( `Empleado con id ${id} no existe`);
     }
    
 }
-
-//console.log(getEmpleado(5));
-getEmpleado(1, (err,empleado)=>{
+const getSalario =(id,callback)=>{
+    const salario = salarios.find(s=>s.id==id)?.salario;
+    if(salario){
+        callback(null, salario);
+    }else{
+        callback(`Salario con id ${id} no existe`);
+    }
+}
+const id=3;
+getEmpleado(id, (err,empleado)=>{
     if(err){
-        console.log('Error')
+        console.log('Error!');
         return console.log(err);
     }
-    console.log('Empleado encontrado');
-    console.log(empleado);
+
+    getSalario(id, (err,salario)=>{
+        if(err){
+            return console.log(err);
+        }
+      
+        console.log('El empleado:', empleado, 'tiene un salario de:', salario);
+    })
 })
