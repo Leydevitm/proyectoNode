9b60625b42a25cdaa8f8b7e43daf209303c81293
leydevitm <leivy.candelaria@devitm.com>
2025-06-05 23:31:41 -0600

    Clase 21 Funciones de Flecha

---
+++ seccion3/Fundamentos/flecha.js
@@ -0,0 +1,17 @@
+
+function sumar2(a, b) {
+  return a + b;
+}   
+console.log(sumar2(5,10)); 
+
+//funciones de flecha 
+//
+const sumar =(a,b) => {
+    return a+ b;
+}
+//mas simplificada
+const sumar3=(a,b) => a + b;
+const saludar =() => 'Hola Mundo';
+
+console.log(sumar(5,10));
+console.log(saludar());
