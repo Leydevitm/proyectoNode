2711da0eb7dd31aba1264fc69758302c57e13cdc
leydevitm <leivy.candelaria@devitm.com>
2025-06-09 09:31:17 -0600

    Clase Numero 31 Inicio del Proyecto

---
+++ seccion4/app.js
@@ -0,0 +1,19 @@
+
+
+for (let index = 1; index <= 10; index++) {
+    const element = index * 5;
+  //  console.log(`${index} * 5 = ${element}`);
+    
+};
+
+
+console.clear();
+console.log('==============');
+console.log('Tabla del 5');
+console.log('==============');
+const base=5;
+for (let index = 1; index <= 10; index++) {
+   
+    console.log(`${base} *${index} = ${base * index}`);
+    
+}
