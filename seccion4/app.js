84f22404803c32f4f6ac555c91ef0db2a0acbd8a
leydevitm <leivy.candelaria@devitm.com>
2025-06-09 14:30:54 -0600

    Requerir Paquetes

--- seccion4/app.js
+++ seccion4/app.js
@@ -1,19 +1,27 @@
 
 
-for (let index = 1; index <= 10; index++) {
-    const element = index * 5;
+//for (let index = 1; index <= 10; index++) {
+   // const element = index * 5;
   //  console.log(`${index} * 5 = ${element}`);
     
-};
+//};
 
-
+const fs=require('fs');
+const base=3;
 console.clear();
 console.log('==============');
-console.log('Tabla del 5');
+console.log(`tabla del ${base}`);
 console.log('==============');
-const base=5;
+
+let salida='';
 for (let index = 1; index <= 10; index++) {
    
-    console.log(`${base} *${index} = ${base * index}`);
+    salida +=(`${base} *${index} = ${base * index}\n`);
     
 }
+console.log(salida)
+fs.writeFile(`tabla-${base}`,salida,(err)=>{
+    if(err) throw err;
+
+    console.log(`tabla-${base} creada`);
+})
